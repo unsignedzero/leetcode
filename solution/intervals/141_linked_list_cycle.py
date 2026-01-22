488d220f326d4d190b58653c05069ea06db53c27
David Tran <unsignedzero@gmail.com>
2026-01-21 22:51:20 -0800

    Adding 141
    Signed-off-by: David Tran <unsignedzero@gmail.com>

---
+++ solution/intervals/141_linked_list_cycle.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        r""" An interesting solution using the slow/faster pointer solution.
+        """
+        slowPointer = fastPointer = head
+
+        while fastPointer and fastPointer.next:
+            slowPointer = slowPointer.next
+            fastPointer = fastPointer.next.next
+            if slowPointer == fastPointer:
+                return True
+        return False
+
+
+    def hasCycle2(self, head: Optional[ListNode]) -> bool:
+        r"""A brute-force solution to check which nodes we have seen
+        """
+
+        seen = set()
+        currentNode = head
+
+        while True:
+            if currentNode is None:
+                break
+            if currentNode in seen:
+                return True
+            seen.add(currentNode)
+            currentNode = currentNode.next
+
+
+        return False
+
