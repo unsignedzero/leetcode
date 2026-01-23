1936b6f1090e176166f0f3a4c55619cc781d1aa1
David Tran <unsignedzero@gmail.com>
2026-01-23 00:24:06 -0800

    Added 104
    Signed-off-by: David Tran <unsignedzero@gmail.com>

---
+++ solution/binary_tree_general/104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+
+        left = right = 0
+        if root.left:
+            left = 1 + self.maxDepth(root.left)
+        if root.right:
+            right = 1 + self.maxDepth(root.right)
+
+        return max(left, right)
+
