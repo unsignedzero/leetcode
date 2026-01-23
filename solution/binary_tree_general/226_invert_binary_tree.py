1f02f5c71c6992fc181979bc945fcc6277ea76f5
David Tran <unsignedzero@gmail.com>
2026-01-23 00:46:56 -0800

    Added 226
    Signed-off-by: David Tran <unsignedzero@gmail.com>

---
+++ solution/binary_tree_general/226_invert_binary_tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        # Reject on an empty node
+        if not root:
+            return
+
+        # If we have a node, root.left and root.right are defined
+        # but values maybe None
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        root.left, root.right = root.right, root.left
+
+        return root
+
