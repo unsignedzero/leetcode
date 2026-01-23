d8984076a5b77e8010be5281ddabe06080f88907
David Tran <unsignedzero@gmail.com>
2026-01-23 01:03:04 -0800

    Adding 101
    Signed-off-by: David Tran <unsignedzero@gmail.com>

---
+++ solution/binary_tree_general/101_symmetric_tree.py
@@ -0,0 +1,39 @@
+from typing import Optional
+# Definition for a binary tree node.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def checkSide(self, rootLeft: Optional[TreeNode], rootRight: Optional[TreeNode]) -> bool:
+
+        # Check root node
+        if not rootLeft and not rootRight:
+            return True
+        if (rootLeft and not rootRight) or (not rootLeft and rootRight):
+            return False
+        if rootLeft.val != rootRight.val:
+            return False
+
+        # Check Left
+        if (not rootLeft.left and rootRight.right) or (rootLeft.left and not rootRight.right):
+            return False
+        left = self.checkSide(rootLeft.left, rootRight.right)
+
+        # Check Right
+        if (not rootLeft.right and rootRight.left) or (rootLeft.right and not rootRight.left):
+            return False
+        right = self.checkSide(rootLeft.right, rootRight.left)
+
+        return all((left, right))
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        if not root or (not root.left and not root.right):
+            return True
+
+        return self.checkSide(root.left, root.right)
+
