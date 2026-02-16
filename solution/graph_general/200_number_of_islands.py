8e1e6b78f3c037999305734f9041e60d0cb0edb1
David Tran <unsignedzero@gmail.com>
2026-02-15 23:26:59 -0800

    Added comments to 200
    Signed-off-by: David Tran <unsignedzero@gmail.com>

--- solution/graph_general/200_number_of_islands.py
+++ solution/graph_general/200_number_of_islands.py
@@ -3,6 +3,10 @@ from typing import List
 
 class Solution:
     def getNeighbors(self, remaining: set(tuple(int)), point: tuple(int)) -> list(tuple(int)):
+        r""" Generates the 4 valid points and checks which ones can be 'visited'
+        and return it back.
+        """
+
         x, y = point
         possibleSet = set(((x-1, y), (x+1, y), (x, y-1), (x, y+1)))
         validOptions = remaining & possibleSet
@@ -13,13 +17,18 @@ class Solution:
         return list(validOptions)
 
     def getAll1(self, grid: List[List[str]]) -> set(tuple(int)):
+        r""" This scans for all valid points (i.e. land which is marked as '1')
+        and makes a proper set of all of them.
+        """
+
         #(((m, n) for n, value in enumerate(row) if value != '0') for m, row in enumerate(grid))
         ret = set()
         for m, row in enumerate(grid):
-           ret.update((m, n) for n, value in enumerate(row) if value != '0')
+            ret.update((m, n) for n, value in enumerate(row) if value != '0')
         return ret
 
     def numIslands(self, grid: List[List[str]]) -> int:
+
         islandCount = 0
         allPoints = self.getAll1(grid)
 
@@ -48,6 +57,6 @@ if __name__ == '__main__':
   ["1","1","0","0","0"],
   ["0","0","0","0","0"]
 ]
-    islandCount = solution.numIslands(grid)
-    print(islandCount)
+    finalIslandCount = solution.numIslands(grid)
+    print(finalIslandCount)
 
